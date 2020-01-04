6e79dc87984a48f0d0780766ef21edc161b4c096
CoachK333 <985482158@qq.com>
2020-01-04 23:41:43 +0800

    jdk7

---
+++ jdk7_source/src/main/java/com/example/jdk7_source/Jdk7SourceApplication.java
@@ -0,0 +1,13 @@
+package com.example.jdk7_source;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jdk7SourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jdk7SourceApplication.class, args);
+	}
+
+}
